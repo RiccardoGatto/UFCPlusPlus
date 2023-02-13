f411df956fa9f00032eae19594d1c0d133c8204a
RiccardoGatto <gatto.riccardo@gmail.com>
2023-02-13 13:05:51 +0100

    Modifiche varie

--- tabellina.cpp
+++ tabellina.cpp
@@ -5,12 +5,12 @@ void stampaTabellina(int x)
     int i = 1;
     while (i <= 10)
     {
-        cout << x << " x " << i << " = " << x * i << "\n";
+        cout << x << "x" << i << " = " << x * i << "\n";
         i++;
     }
 }
 int main()
 {
-    int mioNumero = 158;
+    int mioNumero = 7;
     stampaTabellina(mioNumero);
 }
